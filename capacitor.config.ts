acb443b41117dd240946ce49890d6257a9930c2d
Stany <stanfordbonongwe80@gmail.com>
2023-03-23 13:45:30 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'poultry-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
